19486897f199edd33d6f5c70efdaaa5833886c7c
junhee4180 <junhee4180@naver.com>
2024-09-25 00:33:19 +0900

    Add files via upload

---
+++ semina.py
@@ -0,0 +1,10 @@
+import random
+a ={'1' : random.randint(1,5),'2' : random.randint(1,5),'3' : random.randint(1,5),'4' : random.randint(1,5),'5' : random.randint(1,5),'6' : random.randint(1,5),'7' : random.randint(1,5),'8' : random.randint(1,5),'9' : random.randint(1,5),'10' : random.randint(1,5),'11' : random.randint(1,5),'12' : random.randint(1,5),'13' : random.randint(1,5),'14' : random.randint(1,5),'15' : random.randint(1,5),'16' : random.randint(1,5),'17' : random.randint(1,5),'18' : random.randint(1,5),'19' : random.randint(1,5),'20' : random.randint(1,5),'21' : random.randint(1,5),'22' : random.randint(1,5),'23' : random.randint(1,5),'24' : random.randint(1,5),'25' : random.randint(1,5)}
+print('옳은 답 :',a)
+s1 = [a.values()]
+print(s1)
+print('1의 개수:', s1.count(1))
+print('2의 개수:', s1.count(2))
+print('3의 개수:', s1.count(3))
+print('4의 개수:', s1.count(4))
+print('5의 개수:', s1.count(5))
