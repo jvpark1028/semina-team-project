6038f4193752186b154c1804d16fbd72ad83fae1
jvpark1028 <jvpark1028@gmail.com>
2024-09-27 04:23:36 +0900

    End

---
+++ itisjs.js
@@ -0,0 +1,14 @@
+function clop(x, y) {
+    document.getElementById(x).style.display = y;
+}
+function nigger(z) {
+    document.getElementById('alert').style.display = 'block';
+    document.getElementById('title').innerHTML = z;
+}
+function coolor(b) {
+    document.body.style.background = b;
+}
+function adger(x, y, z) {
+    document.getElementById(x).style.display = 'block';
+    document.getElementById(y).innerHTML = z;
+}
